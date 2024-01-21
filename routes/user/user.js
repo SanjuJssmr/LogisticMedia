5abd93484168d45e15d248d182b7f2d365a408cf
Raveendhar S <raveendhar.s@dokonaly.com>
2024-01-21 21:03:49 +0530

    admin report api's added

--- routes/user/user.js
+++ routes/user/user.js
@@ -1,5 +1,6 @@
 const Router = require('koa-router')
 const { userRegister, updateRegisterData, resendOtp, login, verifyOtp } = require("../../controllers/user/user")
+const { getReportPost, deleteReportedPost } = require('../../controllers/user/admin')
 // const { userRegisterValidation } = require("../../validation/userValidation")
 const userRouter = new Router({ prefix: "/users" })
 
@@ -9,6 +10,8 @@ try {
     userRouter.post("/resendOtp", resendOtp)
     userRouter.post("/login", login)
     userRouter.post("/verifyOtp", verifyOtp)
+    userRouter.get("/getReportPost", getReportPost)
+    userRouter.post("/deleteReportedPost", deleteReportedPost)
 
 } catch (error) {
     console.log(`error in user router - ${error}`);
